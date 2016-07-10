0e391e83b4a70c74633a7cfb97c2f67b154bbeb7
ddddan <d1mullin@gmail.com>
2016-07-10 03:47:41 -0400

    Major Update * Express functionality added * Routes added: 'song-edit', 'song-submit'

---
+++ public/js/songEdit.js
@@ -0,0 +1,27 @@
+/* songEdit.js - Handle Song Editor submission */
+
+
+function updateSubmitStatus(evt) {
+    var e = evt.target,
+        eSubmit = document.getElementById('submit');
+    if (!!e.value) {
+        eSubmit.removeAttribute('disabled');
+    } else {
+        eSubmit.setAttribute('disabled',true);
+    }
+}
+
+function submit(evt) {
+
+}
+
+
+window.onload = function () {
+    var eSongText = document.getElementById('songtext'),
+        eSubmit = document.getElementById('submit');
+
+    // Event listeners
+    eSongText.addEventListener('input', updateSubmitStatus);
+    eSongText.addEventListener('propertychange', updateSubmitStatus);
+    eSubmit.addEventListener('click', submit);
+};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var parseSongtext = require('../middleware/parseSongtext.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
-        title: 'Express'
+        title: 'Songbook Creator'
     });
+});
+
+/* GET Song editor page */
+router.get('/song-edit', function (req, res, next) {
+    res.render('songEdit', {
+        title: 'Songbook Creator - Song Editor'
+    });
+});
+
+/* POST Song editor submission */
+router.post('/song-submit', function (req, res, next) {
+    parseSongtext(req, res, next);
 });
 
 module.exports = router;
